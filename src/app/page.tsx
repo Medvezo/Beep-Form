afd473c26682507c9e371efe6a6add7309ac1038
Mansur Mustafayev <mansur.mustafayev@gmail.com>
2024-05-13 14:41:25 +0400

    refactor: redirect from / to step-1 in /page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
+"use client";
+// It's not the best practice to have page as a client component because of SEO issues but i neglect that here to not create additional container component for that
+import { redirect } from 'next/navigation';
+
 export default function Home() {
-  return (
-    <main> Main Page</main>
-  );
+    redirect("/step-1")
 }
