b1a4d0f8e5f00531b1f3eeab9b85aae9aa556a72
Mansur Mustafayev <mansur.mustafayev@gmail.com>
2024-05-13 20:02:22 +0400

    style: restyle and refactor summary page

--- src/app/summary/page.tsx
+++ src/app/summary/page.tsx
@@ -1,6 +1,9 @@
 "use client";
 // It's not the best practice to have page as a client component because of SEO issues but i neglect that here to not create additional container component for that
 
+import { IoPersonSharp } from "react-icons/io5";
+import { MdEmail } from "react-icons/md";
+import { FaMouse } from "react-icons/fa";
 import FormContext from "@/components/context/FormContext";
 import { useContext } from "react";
 
@@ -8,35 +11,39 @@ export default function Page() {
 	const { state } = useContext(FormContext);
 
 	return (
-		<div className="summary-container">
-			<h1>Summary of Your Input</h1>
-			<div className="summary-section">
-				<h2>Personal Details</h2>
-				<p>
-					<strong>First Name:</strong> {state.personalDetails.firstName}
-				</p>
-				<p>
-					<strong>Last Name:</strong> {state.personalDetails.lastName}
-				</p>
-				<p>
-					<strong>Age:</strong> {state.personalDetails.age}
-				</p>
-			</div>
-			<div className="summary-section">
-				<h2>Contact Information</h2>
-				<p>
-					<strong>Email:</strong> {state.contactInfo.email}
-				</p>
-				<p>
-					<strong>Password:</strong> {state.contactInfo.password}
-				</p>
-			</div>
-			<div className="summary-section">
-				<h2>Usage</h2>
-				<p>
-					<strong>Type:</strong> {state.usage.use}
-				</p>
-			</div>
+		<div className="flex flex-col gap-10  ">
+			<section className="flex flex-col gap-2 justify-center items-center">
+				<div className="flex gap-2 justify-center items-center">
+					<IoPersonSharp className="w-8 h-8 " />
+					<h2 className="text-lg font-semibold">Personal Details</h2>
+				</div>
+				<div className="flex flex-col gap-5 font-bold w-full text-slate-500 text-lg justify-center items-center border border-violet-500 rounded-lg py-2 px-10">
+					<p>First Name: {state.personalDetails.firstName}</p>
+					<p>Last Name: {state.personalDetails.lastName}</p>
+					<p>Birthdate: {state.personalDetails.age}</p>
+				</div>
+			</section>
+
+			<section className="flex flex-col gap-2 justify-center items-center">
+				<div className="flex gap-2 justify-center items-center">
+					<MdEmail className="w-8 h-8" />
+					<h2 className="text-lg font-semibold">Contact Information</h2>
+				</div>
+				<div className="flex flex-col gap-5 font-bold w-full text-slate-500 text-lg justify-center items-center border border-violet-500 rounded-lg py-2 px-10">
+					<p>Email: {state.contactInfo.email}</p>
+					<p>Password: {state.contactInfo.password}</p>
+				</div>
+			</section>
+
+			<section className="flex flex-col gap-2 justify-center items-center">
+				<div className="flex gap-2 justify-center items-center">
+					<FaMouse className="w-8 h-8" />
+					<h2 className="text-lg font-semibold">Usage</h2>
+				</div>
+				<div className="flex flex-col gap-5 font-bold w-full text-slate-500 text-lg justify-center items-center border border-violet-500 rounded-lg py-2 px-10">
+					<p>Type: {state.usage.use}</p>
+				</div>
+			</section>
 		</div>
 	);
 }
